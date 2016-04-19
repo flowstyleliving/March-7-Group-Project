b5e4bc7ac1a268e844b08f7e9f24e7863ab13dfe
tricatseng <heygameover@gmail.com>
2016-04-19 15:04:43 -0700

    fixed fb passport config

--- config/passport.ts
+++ config/passport.ts
@@ -17,21 +17,21 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-  profileFields: ['id','displayName','email'],
-  passReqToCallback: true
-}, function(req, accessToken, refreshToken, profile, next) {
-  User.findOne({'facebook.id': profile.id})
-    .exec((err, user) => {
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+    profileFields: ['id', 'displayName', 'email'],
+    passReqToCallback: true
+  },
+  function(req, accessToken, refreshToken, profile, next) {
+    User.findOne({ 'facebook.id': profile.id }).exec((err, user) => {
       if (err) return next(err);
       if (user) {
         req['tempUser'] = user;
         next(null, user);
       } else {
         let u = new User();
-        u.name = profile.name;
+        u.name = profile.displayName;
         u.email = profile.email;
         u.facebook.id = profile.id;
         u.facebook.token = accessToken;
