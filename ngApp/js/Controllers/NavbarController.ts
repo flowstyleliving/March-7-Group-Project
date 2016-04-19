40d8c741021f25cc7c73e266508a1bac2fa8dcbd
Anthony Urbina <aurbina.metro@gmail.com>
2016-04-19 14:19:51 -0500

    implemented angular material started on nav

--- ngApp/js/Controllers/NavbarController.ts
+++ ngApp/js/Controllers/NavbarController.ts
@@ -1,6 +1,15 @@
 namespace app.Controllers {
     export class NavbarController {
 
+        // public originatorEv;
+        // public openMenu($mdOpenMenu, ev){
+        //     this.originatorEv = ev;
+        //     $mdOpenMenu(ev);
+        // }
+
+        constructor(){
+
+        }
     }
     angular.module('app').controller("NavbarController", NavbarController);
 }
