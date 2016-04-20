d0d32d320ac8b1aff9dc521ec9d732ed8702509d
Anthony Urbina <aurbina.metro@gmail.com>
2016-04-20 18:44:11 -0500

    Got things going on the profile page

--- ngApp/js/Controllers/ProfileController.ts
+++ ngApp/js/Controllers/ProfileController.ts
@@ -2,6 +2,15 @@ namespace app.Controllers {
     export class ProfileController {
         public status;
 
+        public social = [];
+        public socialHold;
+
+        /////////Add socialSite to Array
+        public addSocial(){
+            this.social.push(this.socialHold);
+            this.socialHold = {};
+        }
+
         //////////////$mdDialog(Modal);
         public openDialog () {
             this.$mdDialog.show(
