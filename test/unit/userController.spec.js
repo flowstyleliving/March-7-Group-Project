b72e7bb86c9d8e69743412313478c0b815bb100c
flowstyleliving <mys628@gmail.com>
2016-04-22 14:35:59 -0700

    userContr test cont

--- test/unit/userController.spec.js
+++ test/unit/userController.spec.js
@@ -66,5 +66,62 @@ describe('User Controller', () => {
       };
       controller.login(req, res, next);
     });
+    it('Should call next when pw does not match and return err message', (done) => {
+      UserMock.expects('findOne').withArgs({email: 'test@e.mail'})
+      .chain('exec')
+      .yields(null, {
+        email: 'test@e.mail',
+        comparePassword: function(password, cb) {
+          cb(null, false);
+        }
+      });
+
+      let req = {
+        body: {
+          email: 'test@e.mail'
+        }
+      };
+      let res = {
+        json: function() {
+          throw new Error('WTF BRO, JSON wants to nap!')
+        }
+      };
+      let next = function(err) {
+        err.message.should.equal('Invalid login.');
+        done();
+      };
+      controller.login(req, res, next);
+    });
   });
+
+  describe('register()', () => {
+    it('Should create a new User with a hashed pw and return a jwt token', (done) => {
+      UserMock.expects('create')
+      .yields(null, {
+        generateJWT: function(err, cb) {
+          return 'abc';
+        }
+      });
+      let hashStub = sinon.stub(User.prototype, 'hashPassword')
+      hashStub.yields(null, 'macaroni');
+
+      let req = {
+        body: {
+          password: 'potato'
+        }
+      };
+      let res = {
+        json: function(data) {
+          let u = hashStub.getCalls()[0].args[0];
+          u.should.equal('potato');
+          data.token.should.equal('abc');
+          done();
+        }
+      };
+      let next = function() {
+        throw new Error('Next wants to eat fried macaroni salad!')
+      };
+      controller.register(req, res, next);
+    })
+  })
 });
