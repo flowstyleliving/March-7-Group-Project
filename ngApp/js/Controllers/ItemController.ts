6f8992c5cc2b42ca834b5ea2c4563bd2d0a188cc
tricatseng <heygameover@gmail.com>
2016-04-25 18:50:53 -0700

    added comment functions to item controller

--- ngApp/js/Controllers/ItemController.ts
+++ ngApp/js/Controllers/ItemController.ts
@@ -2,9 +2,26 @@ namespace app.Controllers {
     export class ItemController{
         public item;
         public status;
+        public comment: app.i.IComment;
+
+        public createComment() {
+          this.comment.item = this.comment._id;
+          this.CommentService.create(this.comment).then((res) => {
+            this.item.comments.push(res);
+            this.comment.message = '';
+            this.comment.user = '';
+          });
+        }
+
+        public removeComment(c: app.i.IComment) {
+          this.CommentService.remove(c._id).then(() => {
+            this.item.comments.splice(this.item.comments.indexOf(c),1);
+          });
+        }
 
         constructor(
             private UserService: app.Services.UserService,
+            private CommentService: app.Services.CommentService,
             private $stateParams: ng.ui.IStateParamsService,
             private ItemService: app.Services.ItemService,
             private $state: ng.ui.IStateService
