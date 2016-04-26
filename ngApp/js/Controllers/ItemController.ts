9328e4b0405a9b27d7febf32084d654909b61786
Anthony Urbina <aurbina.metro@gmail.com>
2016-04-26 16:19:05 -0500

    Finished comments in material design, still need to fix image and name

--- ngApp/js/Controllers/ItemController.ts
+++ ngApp/js/Controllers/ItemController.ts
@@ -1,8 +1,13 @@
 namespace app.Controllers {
     export class ItemController {
-        public item;
+        public item: app.i.IItem;
         public status;
+        public isShow = true;
         public comment: app.i.IComment;
+
+        public toggleRight = function() {
+            this.$mdSidenav('right').toggle();
+        };
 
         public createComment() {
             this.comment.item = this.item._id;
@@ -37,7 +42,8 @@ namespace app.Controllers {
             private ItemService: app.Services.ItemService,
             private $state: ng.ui.IStateService,
             private $mdDialog,
-            private $mdMedia
+            private $mdMedia,
+            private $mdSidenav
             ) {
             this.status = UserService.status;
             this.item = ItemService.getOne($stateParams['id']);
