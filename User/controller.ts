2b6f09fcf4da8dd03994b2c51abf64247239bf68
flowstyleliving <mys628@gmail.com>
2016-04-27 10:15:06 -0700

    separated nodemailer

--- User/controller.ts
+++ User/controller.ts
@@ -4,143 +4,134 @@ import * as crypto from 'crypto';
 import * as async from 'async';
 import {User, IUserModel} from './model';
 import {Item, IItemModel} from '../Item/model';
+import {transporter} from '../utils/mailerHelper';
 
-let nodemailer = require('nodemailer');
-let transport = require('nodemailer-smtp-transport');
 
 export function login(req: express.Request, res: express.Response, next: Function) {
-  User.findOne({ email: req.body.email })
-    .exec((err, user) => {
-    if (err) return next(err);
-    if (!user) return next({message: 'Invalid login.'});
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (err) return next(err);
-      if (!isMatch) return next({ message: 'Invalid login.' });
-      else res.json({ token: user.generateJWT() });
+    User.findOne({ email: req.body.email })
+        .exec((err, user) => {
+        if (err) return next(err);
+        if (!user) return next({ message: 'Invalid login.' });
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (err) return next(err);
+            if (!isMatch) return next({ message: 'Invalid login.' });
+            else res.json({ token: user.generateJWT() });
+        });
     });
-  });
 }
 
 export function register(req: express.Request, res: express.Response, next: Function) {
-  let u = new User(req.body);
-  u.hashPassword(req.body.password, (err, hash) => {
-    if (err) return next(err);
-    u.password = hash;
-    User.create(u, (err, user: IUserModel) => {
-      res.json({ token: user.generateJWT() });
+    let u = new User(req.body);
+    u.hashPassword(req.body.password, (err, hash) => {
+        if (err) return next(err);
+        u.password = hash;
+        User.create(u, (err, user: IUserModel) => {
+          if(err) return next(err);
+            res.json({ token: user.generateJWT() });
+        });
     });
-  });
 }
 
 export function forgot(req: express.Request, res: express.Response, next: Function) {
-  User.findOne({email: req.body.email})
-    .exec((err,user) => {
+    User.findOne({ email: req.body.email })
+        .exec((err, user) => {
+        if (err) return next(err);
+        if (!user) return next({ message: 'Invalid user' });
+        if (user) {
+            async.waterfall([
+                function(cb) {
+                    crypto.randomBytes(5, (err, buf) => {
+                        let token = buf.toString('hex');
+                        cb(err, token);
+                    });
+                }, function(token, cb) {
+                    user.resetPasswordToken = token;
+                    user.resetPasswordDate = Date.now() + 3600000;
+                    user.save((err) => {
+                        cb(err, token, user);
+                    });
+                }, function(token, user: app.i.IUser, cb) {
+                    let mailOptions = {
+                        from: 'Folio Team <folioteamcc@gmail.com>',
+                        to: user.email,
+                        subject: 'Folio Password Reset',
+                        html: 'Hey ' + user.name + ',<br><br>' + 'We heard you forgot your password. There are 2 steps to reset:<br>' + '1) Here is your reset token. It expires within an hour from when you requested to reset your password: ' + '<strong>' + token + '</strong><br><br>' + '2) Click on the link below to reset<br>' + 'http://localhost:3000/resetPassword<br><br>' + 'If you did not request a reset, please ignore this email. Your password will not be reset.<br><br>' + 'Have a great day!<br><br>' + 'xo,<br>' + 'The Folio Team'
+                    };
+                    transporter.sendMail(mailOptions, (err) => {
+                        return res.redirect('/');
+                    })
+                }], function(err) {
+                    if (err) return next(err);
+                    return res.redirect('/');
+                })
+        }
+    });
+}
+
+export function resetPassword(req: express.Request, res: express.Response, next: Function) {
+  User.findOne({ resetPasswordToken: req.body.token, resetPasswordDate: {$gt: Date.now()}})
+    .exec((err, user) =>{
       if (err) return next(err);
-      if (!user) return next({message: 'Invalid user'});
+      if (!user) {
+        return next({message: 'Invalid token!'});
+      }
       if (user) {
         async.waterfall([
           function(cb) {
-            crypto.randomBytes(20, (err, buf) => {
-              let token = buf.toString('hex');
-              cb(err, token);
+            user.hashPassword(req.body.password, (err, hash) => {
+              if (err) return next(err);
+              user.password = hash;
+              user.resetPasswordDate = undefined;
+              user.resetPasswordToken = undefined;
+              user.save((err) => {
+                cb(err, user);
+              });
             });
-          }, function(token, cb) {
-            user.resetPasswordToken = token;
-            user.save((err) => {
-              cb(err, token, user);
-            });
-          }, function(token, user: app.i.IUser, cb) {
-            let smtpTransporter = nodemailer.createTransport(transport({
-              service: 'Gmail',
-              auth: {
-                user: process.env.GMAIL_USER,
-                pass: process.env.GMAIL_PASS
-              }
-            }));
+          }, function(user: app.i.IUser, cb) {
             let mailOptions = {
-              from: 'Folio Team <folioteamcc@gmail.com>',
-              to: user.email,
-              subject: 'Folio Password Reset',
-              text: 'Hey ' + user.name + ',\n\n' + 'We heard you forgot your password. Please click on the link below to reset it:\n\n' + 'http://localhost:3000/reset/' + token + '\n\n' + 'If you did not request a reset, please ignore this email. Your password will not be reset.\n\n' + 'Have a great day!\n\n' + 'xo,\n' + 'The Folio Team'
+                from: 'Folio Team <folioteamcc@gmail.com>',
+                to: user.email,
+                subject: 'Your Folio Password Has Been Updated!',
+                html: 'Hey ' + user.name + ',<br><br>' + 'Looks like you were able to update your password!<br><br>' + 'If you did not reset it, please contact the Folio Team right away by replying to this email.<br><br>' + 'Have a great day!<br><br>' + 'xo,<br>' + 'The Folio Team'
             };
-            smtpTransporter.sendMail(mailOptions, (err) => {
-              return res.redirect('/');
+            transporter.sendMail(mailOptions, (err) => {
+                if (err) return next(err);
+                return res.redirect('/');
             })
           }], function(err) {
-            if (err) return next(err);
-            return res.redirect('/');
+              if (err) return next(err);
+              return res.redirect('/');
           })
       }
-    });
-    }
-
-export function reset(req: express.Request, res: express.Response, next: Function) {
-  User.findOne({resetPasswordToken: req.params.token}, (err, user) => {
-    if (err) return next(err);
-    if (!user) return next({message: 'Invalid or expired token'});
-    if (user) {
-      async.waterfall([
-        function(cb) {
-          // user.hashPassword(req.body.password, (err, hash) => {
-          //   if (err) return next(err);
-          //   user.password = hash;
-          // });
-          user.resetPasswordToken = undefined;
-          user.save((err) => {
-            res.json({token: user.generateJWT()});
-            cb(err, user);
-          });
-        }, function(user: app.i.IUser, done) {
-          let smtpTransporter = nodemailer.createTransport(transport({
-            service: 'Gmail',
-            auth: {
-              user: process.env.GMAIL_USER,
-              pass: process.env.GMAIL_PASS
-            }
-          }));
-          let mailOptions = {
-            from: 'Folio Team <folioteamcc@gmail.com>',
-            to: user.email,
-            subject: 'Your Folio Password has been Reset',
-            text: 'Password has been reset!'
-          };
-          smtpTransporter.sendMail(mailOptions, (err) => {
-            return res.redirect('/');
-          })
-        }], function(err) {
-          if (err) return next(err);
-          return res.redirect('/');
-        })
-    }
-  });
+    })
 }
 
 export function findAll(req: express.Request, res: express.Response, next: Function) {
-        User.find({})
+    User.find({})
         .select('-password -facebook')
         .exec((err, data) => {
-            if (err) return next (err);
-            res.json(data);
-        });
-    }
+        if (err) return next(err);
+        res.json(data);
+    });
+}
 
 
 export function findOne(req: express.Request, res: express.Response, next: Function) {
-        User.findOne({_id: req.params.id})
+    User.findOne({ _id: req.params.id })
         .select('-password -facebook')
         .populate('items', 'title images description datePosted dateComplete notes category')
         .exec((err, data) => {
-            if (err) return next (err);
-            Item.populate(data.items,{path:'user',select:'name',model:'User'}, (err, response) => {
-              if (err) return next(err);
-              res.json(data);
-            });
-          });
-      }
-
-export function update (req: express.Request, res: express.Response, next: Function) {
-        User.update({_id: req.params.id}, req.body,(err, numRows) => {
-            if(err) return next(err);
-            res.json({message: "Updated"});
+        if (err) return next(err);
+        Item.populate(data.items, { path: 'user', select: 'name', model: 'User' }, (err, response) => {
+            if (err) return next(err);
+            res.json(data);
         });
-    }
+    });
+}
+
+export function update(req: express.Request, res: express.Response, next: Function) {
+    User.update({ _id: req.params.id }, req.body, (err, numRows) => {
+        if (err) return next(err);
+        res.json({ message: "Updated" });
+    });
+}
---
+++ utils/mailerHelper.ts
@@ -0,0 +1,10 @@
+import nodemailer = require('nodemailer');
+let transport = require('nodemailer-smtp-transport');
+
+export let transporter = nodemailer.createTransport(transport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+    }
+}));
