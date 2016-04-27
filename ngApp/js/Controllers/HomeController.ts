4aba2a344cc67a23650309b72180aa1a73812cba
Anthony Urbina <aurbina.metro@gmail.com>
2016-04-27 14:08:23 -0500

    changed logic on home controller

--- ngApp/js/Controllers/HomeController.ts
+++ ngApp/js/Controllers/HomeController.ts
@@ -14,12 +14,14 @@ namespace app.Controllers {
                 $location.search('');
                 if ($location.hash()) $location.hash('');
             }
-            UserService.getUser(this.status._id).then((data)=>{
-                this.user = data;
-                if(!this.user.aboutMe){
-                    this.$state.go('Create Profile');
-                }
-            });
+            if(this.status._id){
+                UserService.getUser(this.status._id).then((data)=>{
+                    this.user = data;
+                    if(!this.user.aboutMe){
+                        this.$state.go('Create Profile');
+                    }
+                });
+            }
             UserService.getAll().then((res)=>{
                 this.users = res;
             })
