f1c0a598fca02b1b8ca16946455faa450731eeb7
tricatseng <heygameover@gmail.com>
2016-04-28 16:41:51 -0700

    completed sort on portfolio page thx Anthony

--- ngApp/js/Controllers/UserProfileController.ts
+++ ngApp/js/Controllers/UserProfileController.ts
@@ -1,20 +1,35 @@
 namespace app.Controllers{
     export class UserProfileController {
+        public status;
         public user;
         public url = 'fa-facebook-square';
-        public selectedItem;
-        public items;
 
-        public getItems() {
-          console.log('works');
+        public category =[];
+        public selectedCategory;
+
+        public getItems(){
+            this.UserService.getUser(this.status.email).then((res) => {
+                this.user = res;
+                if(this.selectedCategory != "-"){
+                    this.user.items = this.user.items.filter((u)=> {
+                        return u.category == this.selectedCategory
+                    });
+                }
+            })
         }
 
-        constructor(private UserService: app.Services.UserService, private $stateParams: ng.ui.IStateParamsService, private ItemService: app.Services.ItemService){
-            UserService.getUser($stateParams['email']).then((res)=>{
-                this.user = res;
-            });
-
-        };
+        constructor(private UserService: app.Services.UserService, private $stateParams: ng.ui.IStateParamsService){
+          this.status = UserService.status;
+          UserService.getUser(this.status.email).then((res) => {
+              this.user = res;
+              for (let i = 0; i < this.user.items.length; i++){
+                  this.category.push(this.user.items[i].category);
+                  if(this.category.indexOf(this.user.items[i].category) > -1){
+                      continue;
+                  }
+              }
+          });
+        }
     }
     angular.module('app').controller("UserProfileController", UserProfileController);
 }
