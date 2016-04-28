721bed02a5f1b5abd6ce75d7b79c48f65d4621b6
flowstyleliving <mys628@gmail.com>
2016-04-28 11:44:58 -0700

    cleaned up navbar contr

--- ngApp/js/Controllers/NavbarController.ts
+++ ngApp/js/Controllers/NavbarController.ts
@@ -1,7 +1,6 @@
 namespace app.Controllers {
     export class NavbarController {
         public status;
-        public user;
 
         public logout() {
           this.UserService.logout();
@@ -10,10 +9,6 @@ namespace app.Controllers {
 
         constructor(private UserService: app.Services.UserService, private $state: ng.ui.IStateService){
             this.status = UserService.status;
-            UserService.getUser(this.status.email).then((res)=>{
-                this.user = res;
-            })
-            console.log(this.user)
         }
     }
     angular.module('app').controller("NavbarController", NavbarController);
